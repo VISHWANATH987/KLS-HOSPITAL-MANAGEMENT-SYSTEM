f7697e3cdb557c9946cd235bba5b6aab367f74eb
VISHWANATH <71350788+VISHWANATH987@users.noreply.github.com>
2022-02-24 22:48:41 +0530

    Update WelcomeDiv.js

--- src/WelcomeDiv.js
+++ src/WelcomeDiv.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import LocalHospitalRoundedIcon from '@mui/icons-material/LocalHospitalRounded';
 function WelcomeDiv() {
+  axios({
+    method:'get',
+    url:'https://kls-hospital-management-system.herokuapp.com/StartServer',
+    data: {
+            }
+    })
+    .then((response)=> {
+    }, (error) => {
+    });
   return <div className='WelcomeDiv'>
         
         <div className='WelcomeDivContentIcon'> <div className='hospitalIconDiv'><LocalHospitalRoundedIcon className="hospitalIcon"/></div></div>
